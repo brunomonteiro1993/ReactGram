bac5839350f38cb2c3c169ecddceffc13db428eb
brunomonteiro1993 <149431906+brunomonteiro1993@users.noreply.github.com>
2025-02-19 15:49:31 -0300

    EditProfile/foto/bio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,56 @@
-import './App.css'
+import "./App.css";
 
 //Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 //Hooks
-import { useAuth } from './hooks/useAuth';
+import { useAuth } from "./hooks/useAuth";
 
 // components
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 // pages
-import Home from './pages/Home/Home';
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import EditProfile from "./pages/EditProfile/EditProfile";
 
 function App() {
-const {auth, loading} = useAuth()
+  const { auth, loading } = useAuth();
 
-if(loading) {
-  return <p>Carregando...</p>
-}
-
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
-    <div className='App'>
+    <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="container">
-        <Routes>
-          <Route path='/' element={auth ? <Home /> : <Navigate to="/login"/>} />
-          <Route path='/login' element={!auth ? <Login /> : <Navigate to="/"/>} />
-          <Route path='/register' element={!auth ? <Register /> : <Navigate to="/"/>} />
-        </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/profile"
+              element={auth ? <EditProfile /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!auth ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Navigate to="/" />}
+            />
+          </Routes>
         </div>
         <Footer />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
